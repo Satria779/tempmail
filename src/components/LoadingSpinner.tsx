be10d068e6036f50d4290b107aabd5ccc5fdf1bf
Satria779 <satria8640@gmail.com>
2026-06-28 10:39:45 +0700

    Remove unused React import from LoadingSpinner
    Removed unnecessary import statement for React.

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function LoadingSpinner() {
   return (
     <span className="inline-block w-5 h-5 border-2 border-indigo-200 border-t-indigo-600 rounded-full animate-spin" />
