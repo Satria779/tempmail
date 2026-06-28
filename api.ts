507cb169c704ab5ed5ccf3f13eda960be41614fb
Satria779 <satria8640@gmail.com>
2026-06-28 08:58:05 +0700

    Rename api.ts to src/config/api.ts

--- api.ts
+++
@@ -1,1 +0,0 @@
-export const API_BASE = 'https://api-xemoz-official.my.id/api/tools/tempmail.php';
---
+++ src/config/api.ts
@@ -0,0 +1,1 @@
+export const API_BASE = 'https://api-xemoz-official.my.id/api/tools/tempmail.php';
